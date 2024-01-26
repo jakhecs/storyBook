8dbf04f6ed4ea51676bb8537f1e98f1bb8cc5ebb
jakhecs <jakhecs@gmail.com>
2024-01-26 16:19:25 +0000

    Add ESLint ans StoryBook configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
