9a1969a338f00f03f3c3ffe3657d9283c76e2179
jakhecs <jakhecs@gmail.com>
2024-01-29 13:34:57 +0000

    Add Layout Component and update package.json ond vite.config.ts

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export * from "./Buttton";
 export * from "./Input";
-export * from "./Layout/Box";
-export * from "./Layout/Stack";
+// export * from "./Layout/Box";
+// export * from "./Layout/Stack";
+export * from "./Layout";
 export * from "./Text";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "SimpleUI",
+      fileName: "simple-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
